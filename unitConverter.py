e3ab595e426c55745d3ab17bc46553c3c8ef380b
Gage Notargiacomo <125597868+gagenotar@users.noreply.github.com>
2023-04-04 20:10:39 -0400

    Updated the file
    This commit adds instructions for the user and corrects several typos

--- unitConverter.py
+++ unitConverter.py
@@ -14,10 +14,10 @@ class unitConvertor(customtkinter.CTk):
 
         # Sets screen size and title
         self.geometry("500x500")
-        self.title("Unit Convertor")
+        self.title("Unit Converter")
         
         # Presents a welcome message at top of page
-        self.label = customtkinter.CTkLabel(master=self, text="Welcome to the Unit Convertor", font=("Arial", 18, "underline"))
+        self.label = customtkinter.CTkLabel(master=self, text="Welcome to the Unit Converter", font=("Arial", 18, "underline"))
         self.label.pack(pady=10)
 
         # Creates an outer grid structure to hold inner frame and convert button
@@ -25,13 +25,26 @@ class unitConvertor(customtkinter.CTk):
         self.outer_frame.pack()
 
         # Creates multiple tabs to offer different unit types
-        self.tabview = customtkinter.CTkTabview(master=self.outer_frame, height=300, width=350)
+        self.tabview = customtkinter.CTkTabview(master=self.outer_frame, height=100, width=350)
         self.tabview.pack(padx=5, pady=5)
 
         self.tabview.add("Distance")
         self.tabview.add("Mass") 
         self.tabview.add("Volume")
         self.tabview.set("Distance")  # set currently visible tab
+
+        # Creates a set of instructions for the user
+        self.instructions = customtkinter.CTkTextbox(master=self.outer_frame, height=230, width=350, font=("Arial", 13), wrap="word")
+        self.instructions.pack(padx=5, pady=5)
+        self.instructions.insert("0.0", "This is a classic unit converter that can be used to input " 
+                                        "a certain value of one unit, and determine its equivalent value of a different unit.\n\n")
+        self.instructions.insert("3.0", "In order to get started, insert a value in the left textbox.\n")
+        self.instructions.insert("4.0", "Then, use the left drop down menu to determine the units you are converting from.\n")
+        self.instructions.insert("7.0", "Next, select the unit you are converting to by using the right drop down menu.\n")
+        self.instructions.insert("9.0", "To get your equivalent value simply click the convert button.\n")
+        self.instructions.insert("11.0", "To change the type of measurement use the tabs at the top.\n\n")
+        self.instructions.insert("12.0", "Enjoy! :)")
+        self.instructions.configure(state="disabled")
 
         def convertDistanceUnits(self, og_units, new_units, value_in, right):
             # Centimeters will be the universal unit
@@ -150,6 +163,7 @@ class unitConvertor(customtkinter.CTk):
             
             value_in = round(value_in, 3)
 
+            right.configure("disabled")
             right.delete(0, END)
             right.insert("insert", value_in)
 
@@ -165,7 +179,7 @@ class unitConvertor(customtkinter.CTk):
             equals = customtkinter.CTkLabel(master=frame, text="=", font=("Arial", 13, "bold"))
             equals.grid(row=0, column=1, sticky=NSEW, padx=10)
             right = customtkinter.CTkEntry(master=frame, font=("Arial", 13, "bold"), placeholder_text=tempr)
-            right.grid(row=0, column=2, sticky=NSEW, padx=10)
+            right.grid(row=0, column=2, sticky=NSEW, padx=10) 
 
             drop_templ = customtkinter.StringVar(value=menu_options[1])
             drop_tempr = customtkinter.StringVar(value=menu_options[0])
@@ -175,7 +189,6 @@ class unitConvertor(customtkinter.CTk):
                                                 values=menu_options,
                                                 variable=drop_templ)
             dropl.grid(row=1, column=0, sticky=NSEW, padx=10)
-            # # print(drop_left.get()) This statement holds the choice value
             dropr = customtkinter.CTkOptionMenu(master=frame, font=("Arial", 13, "bold"), 
                                                 dropdown_font=("Arial", 13, "bold"),
                                                 values=menu_options,
@@ -188,7 +201,7 @@ class unitConvertor(customtkinter.CTk):
             # Users push this button to convert their units
             convertor = customtkinter.CTkButton(master=frame, text="Convert", font=("Arial", 13, "bold"), 
                                         command=lambda: convert_func(self, dropl.get(), dropr.get(), left.get(), right))
-            convertor.grid(row=2, column=0, sticky=NSEW, padx=10, columnspan=3)           
+            convertor.grid(row=2, column=0, sticky=NSEW, padx=10, columnspan=3)    
 
         distance_values = ["Inch", "Foot", "Yard", "Mile", "Centimeter", "Meter", "Kilometer"]
         mass_values=["Ounce", "Pound", "Ton (US)", "Microgram", "Milligram", "Gram", "Kilogram"]
